13b68ee5e2ebcef32f161b8fcb71564ab060b2f9
jaybooth4 <booth.j@husky.neu.edu>
2016-12-16 13:06:52 -0500

    Change m values

--- js/dataVis.js
+++ js/dataVis.js
@@ -1,5 +1,5 @@
 // Variable instantiation
-var m = [20, 20, 30, 20],
+var m = [10, 10, 50, 10],
     w = 960 - m[1] - m[3],
     h = 500 - m[0] - m[2],
     x,
@@ -12,12 +12,12 @@ var m = [20, 20, 30, 20],
 // Sets color scheme
 var color = d3.scale.category10();
 
-
 var svg = d3.select("body").append("svg")
     .attr("width", w + m[1] + m[3])
     .attr("height", h + m[0] + m[2])
     .append("g")
     .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
 
 // A line generator, for the dark stroke.
 var line = d3.svg.line()
